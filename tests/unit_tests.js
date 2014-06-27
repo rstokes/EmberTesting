624bba787a2a89ec26d30bee28658c3a5ebe7e97
Rob Stokes <rob.stokes@hcahealthcare.com>
2014-06-27 13:27:18 -0500

    moving tree

--- tests/unit_tests.js
+++ tests/unit_tests.js
@@ -2,9 +2,6 @@ test('hello world', function() {
   equal(1, 1, "");
 });
 
-test('hello world', function() {
-  equal(2, 2, "");
-});
 
 test('fullName property returns both first and last', function() {
   var person = App.Person.create({firstName: 'toran', lastName: 'billups'});
