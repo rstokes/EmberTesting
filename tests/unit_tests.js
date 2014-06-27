d857b055f4fce215c6913d42c7274efb0a0a46b9
Rob Stokes <rob.stokes@hcahealthcare.com>
2014-06-27 13:54:34 -0500

    Removing unneeded text.

--- tests/unit_tests.js
+++ tests/unit_tests.js
@@ -2,7 +2,6 @@ test('hello world', function() {
   equal(1, 1, "");
 });
 
-
 test('fullName property returns both first and last', function() {
   var person = App.Person.create({firstName: 'toran', lastName: 'billups'});
   var result = person.get('fullName');
